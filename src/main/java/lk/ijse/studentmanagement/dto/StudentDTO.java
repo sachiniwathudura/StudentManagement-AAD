46a583dfd3a3a043a10000bfc15015e557503980
sachiniwathudura <sachiniwathudura@gmail.com>
2024-07-08 19:44:03 +0530

    create students dto class

---
+++ src/main/java/lk/ijse/studentmanagement/dto/StudentDTO.java
@@ -0,0 +1,21 @@
+package lk.ijse.studentmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class StudentDTO implements Serializable {
+
+    private String id;
+    private  String name;
+    private  String email;
+    private String city;
+    private String level;
+
+}
